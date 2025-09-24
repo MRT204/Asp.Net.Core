77cd8d911ac57bb46a59417a1618f59c1c79e6b5
Asp.Net.Core <dodangtam21@gmail.com>
2025-09-24 08:18:08 +0700

    Mô tả ngắn gọn về thay đổi bạn đã làm

---
+++ lesson01.mvc/Controllers/HomeController1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace lesson01.mvc.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            // Truyền dữ liệu bằng ViewData
+            ViewData["Message"] = "Chào mừng bạn đến với ASP.NET MVC!";
+            ViewData["Date"] = DateTime.Now.ToString("dd/MM/yyyy");
+
+            // Truyền danh sách
+            ViewData["Names"] = new List<string> { "Lan", "Hùng", "Tú", "Mai" };
+
+            return View();
+        }
+    }
+}
---
+++ lesson01.mvc/Models/ViewComponents/CalegoryViewComponents.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing.Design;
+
+namespace lesson01.mvc.Models.ViewComponents
+{
+    public class CalegoryViewComponents
+    {
+        public IViewComponnentResult Invoke()
+        {
+            List<Category> categories = new List<Category>()
+            {
+                new Category {CategoryId = 1, CategoryName = "Điện tử"},
+                new Category {CategoryId = 1, CategoryName = "Điện lạnh"},
+                new Category {CategoryId = 1, CategoryName = "Đồ da dụng"},
+                new Category {CategoryId = 1, CategoryName = "Tiện ích"},
+            };
+            return View(categories);
+        }
+    }
+}
